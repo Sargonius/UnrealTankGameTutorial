a5e68304dc66e2b2a12a5b44d7b717b588dc7e5a
SARGONIUS-Home <evgeniy@rplay.ru>
2017-10-14 23:44:31 +0300

    Pre-refactoring 2

--- TankGame/Source/TankGame/Tank.h
+++ TankGame/Source/TankGame/Tank.h
@@ -9,7 +9,6 @@
 class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
-class UTankMovementComponent;
 class AProjectile;
 
 UCLASS()
@@ -29,9 +28,6 @@ protected:
 
 	UPROPERTY(BlueprintReadOnly)
 	UTankAimingComponent* TankAimingComponent = nullptr;
-
-	UPROPERTY(BlueprintReadOnly)
-	UTankMovementComponent* TankMovement = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
